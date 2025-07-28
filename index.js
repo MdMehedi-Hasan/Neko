20e3d277b4b4116c63f8f861a81e36dec37110ec
MdMehedi-Hasan <mh19115@gmail.com>
2025-07-28 23:58:17 +0600

    product model created

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const Product = require("./models/product.model");
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-const uri = "mongodb+srv://mh19115:xpNrmCImfJzeXJes@mvc1.t7slf8z.mongodb.net/?retryWrites=true&w=majority&appName=mvc1";
+const uri =
+  "mongodb+srv://mh19115:xpNrmCImfJzeXJes@mvc1.t7slf8z.mongodb.net/?retryWrites=true&w=majority&appName=mvc1";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    console.log("Error creating product:", err);
+    res
+      .status(500)
+      .json({ message: "Error creating product", error: err.message });
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
