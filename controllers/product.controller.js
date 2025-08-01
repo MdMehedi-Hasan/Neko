3ada7968a79fed7ff1f4e411ad2a0644e00dd226
MdMehedi-Hasan <mh19115@gmail.com>
2025-08-01 16:53:32 +0600

    all the func's transferred to controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -26,7 +26,55 @@ const getProductById = async (req, res) => {
   }
 };
 
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    console.log("Error creating product:", err);
+    res
+      .status(500)
+      .json({ message: "Error creating product", error: err.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    const updatedProduct = await Product.findById(id);
+    res
+      .status(200)
+      .json({ message: "Product updated successfully", updatedProduct });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error updating product", error: err.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully", product });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error deleting product", error: err.message });
+  }
+};
+
 module.exports = {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
 };
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,55 +1,22 @@
-const express = require('express');
-const Product = require('../models/product.model');
-const { getProducts, getProductById } = require('../controllers/product.controller');
-const router = express.Router()
+const express = require("express");
+const Product = require("../models/product.model");
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller");
+const router = express.Router();
 
 router.get("/", getProducts);
 
 router.get("/:id", getProductById);
 
-router.post("/api/product", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (err) {
-    console.log("Error creating product:", err);
-    res
-      .status(500)
-      .json({ message: "Error creating product", error: err.message });
-  }
-});
+router.post("/", createProduct);
 
-router.patch("/api/product/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    const updatedProduct = await Product.findById(id);
-    res
-      .status(200)
-      .json({ message: "Product updated successfully", updatedProduct });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ message: "Error updating product", error: err.message });
-  }
-});
+router.patch("/:id", updateProduct);
 
-router.delete("/api/product/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    res.status(200).json({ message: "Product deleted successfully", product });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ message: "Error deleting product", error: err.message });
-  }
-});
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
